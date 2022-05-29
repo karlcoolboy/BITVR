db3b123f85ce13a27433744e580b778180a0c315
winsight <75029070+winsight@users.noreply.github.com>
2022-05-29 19:29:58 +0800

    2022-05-29

---
+++ data.js
@@ -0,0 +1,75 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-",
+      "name": "艺术馆",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "yaw": -3.03203120181586,
+        "pitch": -0.046421524441676354,
+        "fov": 1.3961019890881177
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.9162802865404842,
+          "pitch": -0.14479225052484068,
+          "rotation": 0,
+          "target": "1-"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -0.9822084886766049,
+          "pitch": -0.11869510681619388,
+          "title": "Flag<br>",
+          "text": "如果快递进得来，有可能的话做一个整个北理的全景。<br>"
+        }
+      ]
+    },
+    {
+      "id": "1-",
+      "name": "中心花园",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        }
+      ],
+      "faceSize": 200,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
